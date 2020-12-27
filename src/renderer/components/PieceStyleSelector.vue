d77721365300a26b659a634c7409e8618b76600c
Simon Muchau <simon.muchau@freenet.de>
2020-12-27 11:10:08 +0100

    started implementation of style sselecting by variants

--- src/renderer/components/PieceStyleSelector.vue
+++ src/renderer/components/PieceStyleSelector.vue
@@ -1,11 +1,12 @@
 <template>
 <div>
-  <multiselect class="multiselect" v-model="selected" :options="activeStyles" :allow-empty="false" :show-labels="false" :placeholder="selected"></multiselect>
+  <multiselect class="multiselect" v-model="selected" :options="pieceStyles" :allow-empty="false" :show-labels="false" :placeholder="selected"></multiselect>
 </div>
 </template>
 
 <script>
 import Multiselect from 'vue-multiselect'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'pieceStyleSelector',
@@ -14,94 +15,38 @@ export default {
   },
   data () {
     return {
-      styles: {
-        'chess': this.stylesInternational, 'crazyhouse': this.stylesInternational, 'kingofthehill': this.stylesInternational, '3check': this.stylesInternational, 'antichess': this.stylesInternational, 'horde': this.stylesInternational, 'racingkings': this.stylesInternational, 'shogi': this.stylesShogi, 'janggi': this.stylesXiangqi, 'xiangqi': this.stylesXiangqi, 'makruk': this.stylesSEA
-      },
-      stylesInternational: [
-        'alpha',
+      pieceStyles: [
         'california',
-        'cardinal',
-        'cburnett',
-        'chess7',
-        'chessnut',
-        'companion',
-        'dubrovny',
-        'fantasy',
-        'fresca',
-        'gioco',
-        'governor',
-        'icpieces',
-        'kosal',
-        'leipzig',
-        'letter',
-        'libra',
-        'maestro',
         'merida',
-        'pirouetti',
-        'pixel',
-        'reillycraig',
-        'riohacha',
-        'shapes',
-        'spatial',
-        'staunty',
-        'tatiana'
       ],
-      stylesFairy: [
-        'blue',
-        'red',
-        'white'
+      internationalVariants: [
+        'chess'
       ],
-      stylesSEA: [
-        'ada',
-        'cambodian',
-        'intl'
+      internationalPieces: [
+        'alpha',
+        'chess7'
       ],
-      stylesXiangqi: [
-        'ct2',
-        'ct2w',
-        'ct3',
-        'euro',
-        'hanjablue',
-        'hanjagreen',
-        'hnz',
-        'hnzw',
-        'intlblue',
-        'intlgreen',
-        'intlkakao',
-        'intlwooden',
-        'Ka',
-        'Ka_kakao',
-        'Ka_wooden',
-        'playok'
-      ],
-      stylesShogi: [
-        '2kanji',
-        'ctk',
-        'ctkw3d',
-        'ctm',
-        'ctp',
-        'ctp3d',
-        'ctw',
-        'Ka'
-      ],
-      selected: 'merida',
-      activeStyles: this.stylesInternational
+      selected: 'merida'
     }
   },
   computed: {
-    variant () {
-      return this.$store.getters.variant
-    }
+    ...mapGetters(['variant'])
   },
   watch: {
-   
-    selected: function() {
+    selected: function () {
       // e.g. create custom mapping: const pieceStyle = {'alpha': 'alpha', 'tatiana': 'tatiana'};
       console.log(`this.selectedPieceStyle: ${this.selected}`)
       this.$store.dispatch('pieceStyle', this.selected)
     },
-     variant () {
-      this.activeStyles = this.styles[this.variant]
+    variant () {
+      if (this.internationalVariants.includes(this.variant)) {
+        this.selected = this.internationalPieces[0]
+        this.pieceStyles = []
+        this.internationalPieces.forEach(element => {
+          this.pieceStyles.push(element)
+        });
+      }
+
     }
   }
 }
