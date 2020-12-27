0f07a32624ba810f3166aa0bebfbad723d38b0e3
Simon Muchau <simon.muchau@freenet.de>
2020-12-27 11:45:16 +0100

    pieceStyleSelector now implemented with solution w/o store

--- src/renderer/components/PieceStyleSelector.vue
+++ src/renderer/components/PieceStyleSelector.vue
@@ -15,16 +15,108 @@ export default {
   },
   data () {
     return {
-      pieceStyles: [
-        'california',
-        'merida',
-      ],
       internationalVariants: [
-        'chess'
+      'chess', 'crazyhouse', 'horde', 'kingofthehill', '3check', 'racingkings', 'antichess'
+      ],
+      seaVariants: [
+        'makruk'
+      ],
+      xiangqiVariants: [
+        'janggi', 'xiangqi'
+      ],
+      shogiVariants: [
+        'shogi',
+      ],
+      pieceStyles: [
+        'alpha',
+        'california',
+        'cardinal',
+        'cburnett',
+        'chess7',
+        'chessnut',
+        'companion',
+        'dubrovny',
+        'fantasy',
+        'fresca',
+        'gioco',
+        'governor',
+        'icpieces',
+        'kosal',
+        'leipzig',
+        'letter',
+        'libra',
+        'maestro',
+        'merida',
+        'pirouetti',
+        'pixel',
+        'reillycraig',
+        'riohacha',
+        'shapes',
+        'spatial',
+        'staunty',
+        'tatiana'
+      ],
+      shogiPieces: [
+        '2kanji',
+        'ctk',
+        'ctkw3d',
+        'ctm',
+        'ctp',
+        'ctp3d',
+        'ctw',
+        'Ka'
       ],
       internationalPieces: [
         'alpha',
-        'chess7'
+        'california',
+        'cardinal',
+        'cburnett',
+        'chess7',
+        'chessnut',
+        'companion',
+        'dubrovny',
+        'fantasy',
+        'fresca',
+        'gioco',
+        'governor',
+        'icpieces',
+        'kosal',
+        'leipzig',
+        'letter',
+        'libra',
+        'maestro',
+        'merida',
+        'pirouetti',
+        'pixel',
+        'reillycraig',
+        'riohacha',
+        'shapes',
+        'spatial',
+        'staunty',
+        'tatiana'
+      ],
+      seaPieces: [
+        'ada',
+        'cambodian',
+        'intl'
+      ],
+      xiangqiPieces: [
+        'ct2',
+        'ct2w',
+        'ct3',
+        'euro',
+        'hanjablue',
+        'hanjagreen',
+        'hnz',
+        'hnzw',
+        'intlblue',
+        'intlgreen',
+        'intlkakao',
+        'intlwooden',
+        'Ka',
+        'Ka_kakao',
+        'Ka_wooden',
+        'playok'
       ],
       selected: 'merida'
     }
@@ -34,7 +126,6 @@ export default {
   },
   watch: {
     selected: function () {
-      // e.g. create custom mapping: const pieceStyle = {'alpha': 'alpha', 'tatiana': 'tatiana'};
       console.log(`this.selectedPieceStyle: ${this.selected}`)
       this.$store.dispatch('pieceStyle', this.selected)
     },
@@ -46,7 +137,27 @@ export default {
           this.pieceStyles.push(element)
         });
       }
-
+      if (this.shogiVariants.includes(this.variant)) {
+        this.selected = this.shogiPieces[0]
+        this.pieceStyles = []
+        this.shogiPieces.forEach(element => {
+          this.pieceStyles.push(element)
+        });
+      }
+      if (this.seaVariants.includes(this.variant)) {
+        this.selected = this.seaPieces[0]
+        this.pieceStyles = []
+        this.seaPieces.forEach(element => {
+          this.pieceStyles.push(element)
+        });
+      }
+      if (this.xiangqiVariants.includes(this.variant)) {
+        this.selected = this.xiangqiPieces[0]
+        this.pieceStyles = []
+        this.xiangqiPieces.forEach(element => {
+          this.pieceStyles.push(element)
+        });
+      }
     }
   }
 }
