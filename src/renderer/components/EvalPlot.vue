4ca759415005e0d001eadf5bd07744976271e0d1
Unknown <simon.muchau@freenet.de>
2021-01-05 17:26:56 +0100

    Removed Ponder and marked EvalPlot as dummy

--- src/renderer/components/EvalPlot.vue
+++ src/renderer/components/EvalPlot.vue
@@ -22,11 +22,11 @@ export default {
           id: 'vuechart-example'
         },
         xaxis: {
-          categories: [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]
+          categories: ['THIS IS A DUMMY']
         },
         yaxis: {
           title: {
-            text: 'Evaluation [cp]'
+            text: 'THIS IS A DUMMY'
           }
         }
       },
