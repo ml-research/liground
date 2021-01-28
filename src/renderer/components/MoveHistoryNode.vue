6da98744f17a07779aed555fbfe45818baeb27dc
Peter Mader <38166542+ptrmdr@users.noreply.github.com>
2021-01-28 16:12:46 +0100

    Fix line display

--- src/renderer/components/MoveHistoryNode.vue
+++ src/renderer/components/MoveHistoryNode.vue
@@ -4,7 +4,7 @@
     :class="{ variation : isVariation }"
   >
     <span
-      v-if="move.ply % 2 == 1 "
+      v-if="move.ply % 2 == 1"
       class="move-number"
     >
       {{ (move.ply+1) / 2 }}.
@@ -16,7 +16,22 @@
     >
       {{ move.name }}
     </span>
-    <span v-if="move.next">
+    <span v-if="followLine && move.next.length > 1">
+      <move-history-node
+        :move="move.main"
+        :follow-line="false"
+      />
+      <move-history-node
+        v-for="variation in filteredNext"
+        :key="variation.fen"
+        :move="variation"
+      />
+      <move-history-node
+        v-if="move.main.main"
+        :move="move.main.main"
+      />
+    </span>
+    <span v-else-if="followLine">
       <move-history-node
         v-for="variation in move.next"
         :key="variation.fen"
@@ -33,12 +48,21 @@ export default {
     move: {
       default: undefined,
       type: Object
+    },
+    followLine: {
+      default: true,
+      type: Boolean
     }
   },
   computed: {
     isVariation () {
       console.log(this.move)
       return this.move.prev && this.move.fen !== this.move.prev.main.fen
+    },
+    filteredNext () {
+      return this.move.next.filter((variation) => {
+        return variation.fen !== this.move.main.fen
+      })
     }
   },
   methods: {
