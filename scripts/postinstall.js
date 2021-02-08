9a18d409bda69d879505f9fe592eeabfd74cac63
Zerthox <zerthox@gmail.com>
2021-02-08 16:21:38 +0100

    Add postinstall script

---
+++ scripts/postinstall.js
@@ -0,0 +1,92 @@
+const path = require('path')
+const fs = require('fs')
+const https = require('https')
+const os = require('os')
+
+/**
+ * Downloads a file to the given path.
+ * @param {string} url URL to download form
+ * @param {string} filePath Path to file
+ * @returns
+ */
+function downloadFile (url, filePath) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      if (res.statusCode === 302) {
+        // follow the redirect (we need this for downloading from github releases)
+        downloadFile(res.headers.location, filePath).then(resolve).catch(reject)
+        return
+      } else if (res.statusCode !== 200) {
+        reject(new Error(`Server responded with a status of ${res.statusCode} (${res.statusMessage})`))
+        return
+      }
+
+      // pipe the result into the file
+      const file = fs.createWriteStream(filePath)
+      res.pipe(file)
+        .on('finish', () => {
+          file.close(() => resolve())
+        })
+        .on('error', (err) => {
+          fs.unlink(file)
+          reject(err)
+        })
+    })
+  })
+}
+
+/**
+ * Downloads an engine binary.
+ * @param {string} fileName Name of binary to save
+ * @param {Object} urlMap Map of binary URLs for each OS
+ * @param {string} urlMap.mac URL for MacOS binary
+ * @param {string} urlMap.win URL for Windows binary
+ * @param {string} urlMap.linux URL for Linux binary
+ */
+async function downloadBinary (fileName, urlMap) {
+  // find out which file to download
+  let url
+  const platform = os.platform()
+  switch (platform) {
+    case 'darwin':
+      url = urlMap.mac
+      break
+    case 'win32':
+      url = urlMap.win
+      fileName += '.exe'
+      break
+    default:
+      url = urlMap.linux
+  }
+
+  // check if no url available
+  if (!url) {
+    console.error(`No prebuilt binary of ${fileName} available for your current OS. Please build one yourself.`)
+    return
+  }
+  console.log(`Downloading ${fileName} binary from ${url}...`)
+
+  // download file
+  const filePath = path.resolve(__dirname, '../engines', fileName)
+  try {
+    await downloadFile(url, filePath)
+  } catch (err) {
+    console.error(`Failed to download ${fileName}:`, err)
+    return
+  }
+
+  // make the file executable
+  if (platform !== 'win32') {
+    fs.chmod(filePath, '+x', (err) => {
+      if (err) throw err
+      console.log(`Download of ${fileName} completed!`)
+    })
+  } else {
+    console.log(`Download of ${fileName} completed!`)
+  }
+}
+
+downloadBinary('fairy-stockfish', {
+  linux: 'https://github.com/ianfab/Fairy-Stockfish/releases/download/fairy_sf_11_2/fairy-stockfish-largeboard_x86-64',
+  win: 'https://github.com/ianfab/Fairy-Stockfish/releases/download/fairy_sf_11_2/fairy-stockfish-largeboard_x86-64.exe'
+})
