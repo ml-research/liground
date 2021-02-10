b4f750824070b1f04c6492f1057f796a09473809
Peter Mader <38166542+ptrmdr@users.noreply.github.com>
2021-02-10 20:49:07 +0100

    Show all option types and add button to change settings

--- src/renderer/components/SettingsTab.vue
+++ src/renderer/components/SettingsTab.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="about">
     <title>SettingsTab</title>
-    <h1>This is the Settings Tab</h1>
+    <h1 @click="changeTab">
+      This is the Settings Tab
+    </h1>
     <div>
       <table>
         <tr
@@ -11,12 +13,14 @@
           <td> {{ option.name }} </td>
           <td> {{ option.type }} </td>
           <td v-if="option.type === 'combo'">
-            <select size="1">
+            <select
+              v-model="engineSettingsForm[option.name]"
+              size="1"
+            >
               <option
                 v-for="selOption in option.var"
                 :key="selOption"
                 :value="selOption"
-                :selected="selOption === option.default"
               >
                 {{ selOption }}
               </option>
@@ -24,25 +28,35 @@
           </td>
           <td v-else-if="option.type === 'check'">
             <input
+              v-model="engineSettingsForm[option.name]"
               type="checkbox"
-              :checked="option.default === 'true'"
               :name="option.name"
             >
           </td>
           <td v-else-if="option.type === 'spin'">
             <input
+              v-model="engineSettingsForm[option.name]"
               type="number"
               :step="1"
-              :value="option.default"
               :min="option.min"
               :max="option.max"
             >
           </td>
-          <td v-else>
-            {{ option.min }} {{ option.max }}
+          <td v-else-if="option.type === 'string'">
+            <input
+              v-model="engineSettingsForm[option.name]"
+              type="text"
+              :name="option.name"
+            >
+          </td>
+          <td v-else-if="option.type === 'button'">
+            <button type="button">
+              {{ option.name }}
+            </button>
           </td>
         </tr>
       </table>
+      <a @click="changeSettings()">Change Settings</a>
     </div>
   </div>
 </template>
@@ -54,12 +68,28 @@ export default {
   name: 'SettingsTab',
   components: {
   },
+  data: function () {
+    return {
+      engineSettingsForm: {}
+    }
+  },
   computed: {
     ...mapGetters(['engineOptions'])
+  },
+  created: function () {
+    for (const option of this.engineOptions) {
+      if (option.type !== 'button') {
+        this.engineSettingsForm[option.name] = option.type === 'check' ? option.default === 'true' : option.default
+      }
+    }
   },
   methods: {
     changeTab () {
       this.$store.commit('viewAnalysis', !this.$store.getters.viewAnalysis)
+      console.log(this.engineOptions[0])
+    },
+    changeSettings () {
+      this.$store.dispatch('setEngineOptions', this.engineSettingsForm)
     }
   }
 }
