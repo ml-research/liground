0f65482701caaf22ebc9dad0b21783d59fb2377c
Zerthox <zerthox@gmail.com>
2021-03-09 17:38:52 +0100

    Add preview images to piece style selector

--- src/renderer/components/PieceStyleSelector.vue
+++ src/renderer/components/PieceStyleSelector.vue
@@ -6,8 +6,45 @@
       :options="pieceStyles"
       :allow-empty="false"
       :show-labels="false"
-      :placeholder="selected"
-    />
+      placeholder="Piece Style"
+    >
+      <template
+        slot="option"
+        slot-scope="props"
+      >
+        <div class="item">
+          <div class="preview">
+            <div
+              class="image white"
+              :style="{ backgroundImage: preview(props.option)[0] }"
+            />
+            <div
+              class="image black"
+              :style="{ backgroundImage: preview(props.option)[1] }"
+            />
+          </div>
+          <span class="name">{{ props.option }}</span>
+        </div>
+      </template>
+      <template
+        slot="singleLabel"
+        slot-scope="props"
+      >
+        <div class="item">
+          <div class="preview">
+            <div
+              class="image white"
+              :style="{ backgroundImage: preview(props.option)[0] }"
+            />
+            <div
+              class="image black"
+              :style="{ backgroundImage: preview(props.option)[1] }"
+            />
+          </div>
+          <span class="name">{{ props.option }}</span>
+        </div>
+      </template>
+    </Multiselect>
   </div>
 </template>
 
@@ -134,33 +171,30 @@ export default {
     ...mapGetters(['variant'])
   },
   watch: {
-    selected: function () {
-      console.log(`this.selectedPieceStyle: ${this.selected}`)
+    selected () {
       this.$store.dispatch('pieceStyle', this.selected)
     },
     variant () {
+      // TODO: we could add getters like `isInternational()` to the store
       if (this.internationalVariants.includes(this.variant)) {
         this.selected = this.internationalPieces[18]
         this.pieceStyles = []
         this.internationalPieces.forEach(element => {
           this.pieceStyles.push(element)
         })
-      }
-      if (this.shogiVariants.includes(this.variant)) {
+      } else if (this.shogiVariants.includes(this.variant)) {
         this.selected = this.shogiPieces[0]
         this.pieceStyles = []
         this.shogiPieces.forEach(element => {
           this.pieceStyles.push(element)
         })
-      }
-      if (this.seaVariants.includes(this.variant)) {
+      } else if (this.seaVariants.includes(this.variant)) {
         this.selected = this.seaPieces[0]
         this.pieceStyles = []
         this.seaPieces.forEach(element => {
           this.pieceStyles.push(element)
         })
-      }
-      if (this.xiangqiVariants.includes(this.variant)) {
+      } else if (this.xiangqiVariants.includes(this.variant)) {
         this.selected = this.xiangqiPieces[0]
         this.pieceStyles = []
         this.xiangqiPieces.forEach(element => {
@@ -168,10 +202,80 @@ export default {
         })
       }
     }
+  },
+  methods: {
+    preview (name) {
+      let pieces = ['', '']
+      if (this.internationalVariants.includes(this.variant)) {
+        pieces = [`international/${name}/wN`, `international/${name}/bN`]
+      } else if (this.seaVariants.includes(this.variant)) {
+        switch (name) {
+          case 'adarb':
+            pieces = ['southeastasian/ada/orig/rN', 'southeastasian/ada/orig/bN']
+            break
+          case 'adawb':
+            pieces = ['southeastasian/ada/orig/wN', 'southeastasian/ada/orig/bN']
+            break
+          case 'adawr':
+            pieces = ['southeastasian/ada/orig/wN', 'southeastasian/ada/orig/rN']
+            break
+          case 'intl':
+            pieces = ['international/merida/wN', 'international/merida/bN']
+            break
+          default:
+            pieces = [`southeastasian/${name}/wN`, `southeastasian/${name}/bN`]
+        }
+      } else if (this.shogiVariants.includes(this.variant)) {
+        pieces = [`shogi/${name}/0KE`, `shogi/${name}/1KE`]
+      } else if (this.xiangqiVariants.includes(this.variant)) {
+        switch (name) {
+          case 'ct2':
+            pieces = ['xiangqi/ct2/red_horse2', 'xiangqi/ct2/black_horse2']
+            break
+          case 'hanjablue':
+          case 'intlblue':
+          case 'intlkakao':
+          case 'intlwooden':
+          case 'Ka_kakao':
+          case 'Ka_wooden':
+            pieces = [`xiangqi/${name}/red_horse`, `xiangqi/${name}/blue_horse`]
+            break
+          case 'hanjagreen':
+            pieces = ['xiangqi/hanjablue/red_horse', 'xiangqi/hanjagreen/blue_horse']
+            break
+          case 'intlgreen':
+            pieces = ['xiangqi/intlblue/red_horse', 'xiangqi/intlgreen/blue_horse']
+            break
+          default:
+            pieces = [`xiangqi/${name}/red_horse`, `xiangqi/${name}/black_horse`]
+        }
+      }
+      return [`url(static/piece/${pieces[0]}.svg)`, `url(static/piece/${pieces[1]}.svg)`]
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.item {
+  display: flex;
+  flex-direction: row;
+  font-size: 0.9em;
+}
+.item .preview {
+  margin: -8px 4px -8px -4px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.item .image {
+  width: 35px;
+  height: 35px;
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+.item .image + .image {
+  margin-left: 2px;
+}
 </style>
