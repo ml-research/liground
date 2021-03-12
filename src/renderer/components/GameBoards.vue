d0a2db258c542aded7294305e39cf3662f3dfc70
Zerthox <zerthox@gmail.com>
2021-03-12 14:18:55 +0100

    Fix board & eval bar alignment

--- src/renderer/components/GameBoards.vue
+++ src/renderer/components/GameBoards.vue
@@ -4,14 +4,16 @@
       <div class="main-grid">
         <div class="chessboard-grid">
           <PgnBrowser id="pgnbrowser" />
-          <div @mousewheel.prevent="scroll($event)">
-            <ChessGround
-              id="chessboard"
-              :orientation="orientation"
-              @onMove="showInfo"
-            />
+          <div class="board">
+            <div @mousewheel.prevent="scroll($event)">
+              <ChessGround
+                id="chessboard"
+                :orientation="orientation"
+                @onMove="showInfo"
+              />
+            </div>
+            <EvalBar class="evalbar" />
           </div>
-          <EvalBar id="evalbar" />
           <div id="fen-field">
             FEN <input
               id="lname"
@@ -266,23 +268,22 @@ export default {
 <style scoped>
 .main-grid {
   display: grid;
-  grid-template-columns: 3fr 2fr;
+  grid-template-columns: auto auto;
   grid-template-rows: auto auto;
   grid-template-areas:
     "chessboard analysisview"
     "evalplot analysisview";
 }
-.main-grid > .chessboard-grid {
+.chessboard-grid {
   min-width: 925px;
   grid-area: chessboard;
   display: grid;
-  grid-template-columns: 20% 70% auto;
-  grid-template-rows: auto auto auto auto;
+  grid-template-columns: 20% auto;
+  grid-template-rows: auto auto auto;
   grid-template-areas:
-    "pgnbrowser . ."
-    ". fenfield ."
-    ". selector ."
-    ". evalplot .";
+    "pgnbrowser ."
+    ". fenfield"
+    ". selector";
 }
 #analysisview {
   height: 100%;
@@ -315,20 +316,17 @@ input {
   margin-top: 10px;
   width: 210px;
 }
-.main-grid {
-  display: grid;
-  grid-template-columns: auto auto;
-}
-.chessboard-grid {
-  display: grid;
-  grid-template-columns: 20% auto auto;
-}
 #pgnbrowser {
   grid-area: pgnbrowser;
   border: 1px solid black;
   border-radius: 4px;
   margin-left: 1em;
   max-height: 60vh;
+}
+.board {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 }
 #chessboard {
   display: inline-block;
@@ -340,11 +338,11 @@ input {
   display: table;
   margin: 0 auto;
 }
+.evalbar {
+  margin-left: 8px;
+}
 #analysisview {
   margin-left: 15px;
-}
-#evalbar {
-  float: left;
 }
 #evalplot {
   grid-area: evalplot;
