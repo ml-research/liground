c1387d70d5fa00029b12e7e2f6f24e5de45e1c61
Zerthox <zerthox@gmail.com>
2021-03-13 15:25:01 +0100

    Add center marking to eval bar

--- src/renderer/components/EvalBar.vue
+++ src/renderer/components/EvalBar.vue
@@ -23,12 +23,24 @@ export default {
 
 <style scoped>
 .progress {
+  position: relative;
   width: 17px;
   height: 600px;
   background: #fff;
   border: 1px solid #888;
   border-radius: 5px;
   overflow: hidden;
+}
+.progress::before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 0;
+  right: 0;
+  height: 2px;
+  margin-top: -1px;
+  background: #ff4400;
+  opacity: .7;
 }
 .flip {
   transform: scaleY(-1);
