fcb3f8e1343b15dcc513ea96acd51305658cf6bf
Martin Ruzicka <martin.ruzicka@stud.tu-darmstadt.de>
2021-03-16 09:39:02 +0100

    now always displays the move, also when its blacks turn in the fen

--- src/renderer/components/MoveHistoryNode.vue
+++ src/renderer/components/MoveHistoryNode.vue
@@ -9,7 +9,7 @@
       {{ (move.ply+1) / 2 }}.
     </span>
     <span
-      v-else-if="move.ply % 2 === 0 && printRoot
+      v-else-if="(move === mainFirstMove) || move.ply % 2 === 0 && printRoot
         &&(beginVariation || move.prev && (move.prev.prev && move.prev.prev.main === move.prev && move.prev.prev.next.length > 1)
           || (firstMoves.includes(move.prev) && firstMoves.length > 1 && mainFirstMove.next.length > 0 && (move.prev === mainFirstMove || move.prev.main !== move) )
           && move.prev.main === move)"
@@ -278,6 +278,7 @@ export default {
       this.$store.dispatch('deleteFromMoves', move)
     },
     updateBoard (move) {
+      console.log(this.mainFirstMove)
       this.$store.dispatch('fen', move.fen)
       for (const num in this.moves) {
         if (this.moves[num].current) {
