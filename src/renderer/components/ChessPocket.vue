41c24746616bc9707de321fa78e803e6ce4bcfe0
ksthicke <85892703+ksthicke@users.noreply.github.com>
2024-05-12 18:45:46 -0500

    fix crazyhouse drag issue

--- src/renderer/components/ChessPocket.vue
+++ src/renderer/components/ChessPocket.vue
@@ -14,6 +14,7 @@
         class="piece"
         :class="[piece.type, color, piece.type === selectedPiece ? 'selected' : '' , orientation === 'white' && color === 'black' ? 'flipB' : '', orientation === 'black' ? 'flipW' : '']"
         @mousedown="clicked($event, piece.type, color, piece.count)"
+        @dragstart="(e) => e.preventDefault()"
       />
       <div
         class="piece-count noselect"
