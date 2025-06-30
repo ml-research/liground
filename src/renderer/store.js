ddef1667368d9ceb39637ae54a6ea22d821496f2
stjer <58432049+stjer@users.noreply.github.com>
2025-06-30 17:11:33 +0900

    Update store.js
    update for janggi variants

--- src/renderer/store.js
+++ src/renderer/store.js
@@ -141,6 +141,9 @@ export const store = new Vuex.Store({
       Makruk: 'makruk',
       Shogi: 'shogi',
       Janggi: 'janggi',
+      Janggicasual: 'janggicasual',
+      Janggimodern: 'janggimodern',
+      Janggitraditional: 'janggitraditional',
       Xiangqi: 'xiangqi',
       Fischerandom: 'fischerandom'
 
@@ -210,7 +213,7 @@ export const store = new Vuex.Store({
       '+ Add Custom', 'xiangqi'
     ],
     janggiVariants: [
-      '+ Add Custom', 'janggi'
+      '+ Add Custom', 'janggi', 'janggicasual', 'janggimodern', 'janggitraditional'
     ],
     shogiVariants: [
       '+ Add Custom', 'shogi'
@@ -1429,7 +1432,7 @@ export const store = new Vuex.Store({
         return 0
       } else {
         const var2Dim = {
-          shogi: 1, xiangqi: 3, janggi: 3, makruk: 0
+          shogi: 1, xiangqi: 3, janggi: 3, makruk: 0, janggicasual: 3, janggimodern: 3, janggitraditional: 3
         }
         return var2Dim[state.variant]
       }
