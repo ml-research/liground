c47fd7b5ff716ca46bbb5da1911115ec07a7c426
Henri Hormel <henrihormel@gmail.com>
2026-01-23 14:48:58 +0100

    add insert and get statement to helper

--- src/main/evalCache.js
+++ src/main/evalCache.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import { app } from 'electron'
 import Database from 'better-sqlite3'
+import { store } from '../renderer/store'
 
 const dbPath = path.join(app.getPath('userData'), 'cache.db')
 const db = new Database('cache.db', { verbose: console.log })
@@ -22,4 +23,31 @@ export function createSchema () {
     const ddl = db.prepare(CREATE_EVAL_CACHE_TABLE);
     const ddlInfo = ddl.run()
     db.exec("INSERT OR IGNORE INTO eval_cache VALUES ('abcdef', 'stockfish', '1.0', '20', '10.3','988', '4.e3 e7', '1313231')")
+}
+
+export function insertEval (evaluation) {
+    engineName = evaluation.engineName.split(" ")[0]
+    engineVersion = evaluation.engineName.split(" ")[1]
+    const insert = db.prepare(`
+    INSERT INTO eval_cache VALUES (
+    '${evaluation.positionKey}',
+    '${engineName}',
+    '${engineVersion}',
+    '${evaluation.depth}',
+    '${evaluation.cp}',
+    '${evaluation.pv}',
+    '${evaluation.updatedAt}'
+    );
+    `);
+    const insertInfo = insert.run() 
+}
+
+export function getEval (position_key) {
+    const select = db.prepare(`
+    SELECT LIMIT 1 FROM eval_cache WHERE
+    position_key = '${evaluation.positionKey}'
+    ;
+    `);
+    const result = select.get()
+    return result
 }
