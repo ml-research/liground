834a999cadfc8d46d5bec109b7a63896f6ad78d7
Henri Hormel <henrihormel@gmail.com>
2026-01-29 15:16:31 +0100

    add depth, seldepth and wdl in enginesender

--- src/renderer/engine/sender.js
+++ src/renderer/engine/sender.js
@@ -75,7 +75,7 @@ export default class EngineSender {
       case 'info':
         if ('multipv' in payload) {
           const line = {}
-          for (const key of ['multipv', 'pv', 'cp', 'mate']) {
+          for (const key of ['multipv', 'pv', 'cp', 'wdl', 'mate', 'depth', 'seldepth']) {
             line[key] = payload[key]
             delete payload[key]
           }
